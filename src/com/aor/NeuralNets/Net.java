e58a04dca68b5557e8c490c97e42ad88b86c1e74
Josh Hofing <joshhofing@gmail.com>
2012-10-05 11:03:49 -0400

    So much refactoring....

--- src/com/aor/NeuralNets/Net.java
+++ src/com/aor/NeuralNets/Net.java
@@ -9,7 +9,9 @@ public class Net {
     private ArrayList<InputNode> _inputs;
     private ArrayList<OutputNode> _outputs;
     private ArrayList<Link> _links;
-    private int _topId = 0;
+    private int _topId;
+    private int _netLength;
+    private int _netWidth;
     
 
     public Net () {
@@ -17,10 +19,31 @@ public class Net {
         _inputs = new ArrayList<InputNode>();
         _outputs = new ArrayList<OutputNode>();
         _links = new ArrayList<Link>();
+        _topId = 0;
+        _netLength = 4;
+        _netWidth = 4;
         generateNet();
         //for (int i = 0; i < 10; i++) {
             runNet();
         //}
+    }
+
+    public Net clone (Net original) {
+        Net newNet = new Net();
+    
+        //first the simple variables
+        newNet.setLength(getLength());
+        newNet.setWidth(getWidth());
+
+        //then, lets clone all the nodes...
+        for (Node node : getNodes()) {
+            newNet.add(node.clone());
+        }
+        // And then clone inputs/outputs
+        for (InputNode node : _inputs) {
+            _
+        }
+
     }
 
     /*
@@ -35,25 +58,25 @@ public class Net {
         ArrayList<Node> secondLayer = new ArrayList<Node>();
         
         // populate the first layer
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < _netWidth; i++) {
             InputNode firstNode = new InputNode(getNextId());
 
             firstLayer.add(firstNode);
             _inputs.add(firstNode);
-            _nodes.add(firstNode);
+            addNode(firstNode);
     
         }
         
         
-        for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < _netLength; j++) {
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < _netWidth; i++) {
 
                 NodeOps OpChoice = NodeOps.values()[NeuralNets.generator.nextInt(NodeOps.values().length)];
                 Node secondNode = new Node(getNextId(), OpChoice);
 
                 secondLayer.add(secondNode);
-                _nodes.add(secondNode);
+                addNode(secondNode);
 
             }
 
@@ -70,7 +93,7 @@ public class Net {
         for (int i = 0; i < 1; i++) { //Node node : firstLayer) {
             OutputNode newOutput = new OutputNode(getNextId());
             _outputs.add(newOutput);
-            _nodes.add(newOutput);
+            addNode(newOutput);
         }
         for (Node node : firstLayer) {
             for (Node output : _outputs) {
@@ -83,7 +106,7 @@ public class Net {
 
     public void runNet () {
         // Test sending something in
-        double startingInput = 2.0;//NeuralNets.generator.nextInt(100);
+        double startingInput = NeuralNets.generator.nextInt(100); //Start at a random int, so the net doesn't evolve to just spit out the "right" answer for a given seed, rather than actually thinking it through.
         double workingInput = startingInput;
         for (Node input : _inputs) {
             input.recieveTrigger(workingInput);
@@ -118,7 +141,8 @@ public class Net {
         }
         
         double output = _outputs.get(0).getOutput();
-        
+        // Well, lets see if we can optimize this net. 
+        // TODO: Switch to evolutionary method.
         if (output != targetValue) {
             double delta = targetValue - output;
             double gradient = delta * startingInput;
@@ -126,9 +150,11 @@ public class Net {
             System.out.println("Delta: " + delta);
             System.out.println("Gradient: " + gradient);
             ArrayList<Link> links = _outputs.get(0).getInputs();
+
             for (Link link : _links) {
                 link.setWeight( link.getWeight() + delta );
             }
+
             System.out.println("Output from net: " + output);
         }
 
@@ -143,6 +169,41 @@ public class Net {
         _topId++;
         System.out.println("Assigning ID: " + _topId);
         return _topId;
+    }
+
+    public int getWidth () {
+        return _netWidth;
+    }
+    public void setWidth (int width) {
+        _netWidth = width;
+    }
+
+    public int getLength () {
+        return _netLength;
+    }
+    public void setLength (int length) {
+        _netLength = length;
+    }
+
+    public ArrayList<Node> getNodes () {
+        return _nodes;
+    }
+    public void addNode (Node node) {
+        _nodes.add(node);
+    }
+
+    public ArrayList<InputNode> getInputs () {
+        return _inputs;
+    }
+    public void addInput (InputNode node) {
+        _inputs.add(node);
+    }
+
+    public ArrayList<OutputNode> getOutputs () {
+        return _outputs;
+    }
+    public void addOutput (OutputNode node) {
+        _outputs.add(node);
     }
 
 
